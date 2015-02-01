f8a505f119d5a0b8dd2f6d09a63977afcffe6472
cat <cat@catdeMacBook-Pro.local>
2015-02-01 12:33:49 +0800

    First step for release

---
+++ block.js
@@ -0,0 +1,5 @@
+var adElement = document.getElementById('player-ad'),
+  footer = document.getElementById('footer');
+
+adElement.style.display = 'none';//屏蔽广告
+footer.style.height = '40px';//将底部的高度设置40px，去掉广告部分的高度。
