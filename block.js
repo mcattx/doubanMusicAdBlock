e725dd805be070886538043ab64bf4936daa0325
cat <cat@catdeMacBook-Pro.local>
2015-02-07 16:13:46 +0800

    修改block.js，增加正则表达式以判断包含ad的div标签

--- block.js
+++ block.js
@@ -1,5 +1,28 @@
-var adElement = document.getElementById('player-ad'),
-  footer = document.getElementById('footer');
+var footer = document.getElementById('footer'),
+  allDiv = document.getElementsByTagName('div');
+  adRegex = /\w+-ad|ad-/gi;
 
-adElement.style.display = 'none';//屏蔽广告
-footer.style.height = '40px';//将底部的高度设置40px，去掉广告部分的高度。
+var divArray = [],//存在id属性的div标签数组
+  adArray = [];//广告div数组
+
+  for (var i = allDiv.length - 1; i >= 0; i--) {
+    if (allDiv[i].getAttribute("id")) {
+      divArray.push(allDiv[i].getAttribute("id"));
+    };
+  };
+  console.log(divArray);
+
+  for (var i = divArray.length - 1; i >= 0; i--) {
+    if (adRegex.test(divArray[i])) {
+      adArray.push(divArray[i]);
+    };
+  };
+  console.log(adArray);
+
+  for (var i = adArray.length - 1; i >= 0; i--) {
+    document.getElementById(adArray[i]).style.display = 'none';
+  };
+
+  footer.style.height = '40px';//将底部的高度设置40px，去掉广告部分的高度。
+
+
